4b2e595e19fcd60083efd9077f0203dac85a0869
krawaster <krawaster@gmail.com>
2023-03-06 18:54:32 -0500

    76. Minimum Window Substring

--- src/SlidingWindow/minimumWindowSubstring/Solution.php
+++ src/SlidingWindow/minimumWindowSubstring/Solution.php
@@ -2,12 +2,17 @@
 
 namespace SlidingWindow\minimumWindowSubstring;
 
+/**
+ * https://leetcode.com/problems/minimum-window-substring/description/
+ */
 class Solution
 {
     public function __construct()
     {
-        $this->minWindow('s', 'w');
-        echo "test";
+        $s = "ADOBECODEBANC";
+        $t = "ABC";
+        $result = $this->minWindow($s, $t);
+//        $result = $this->minWindowExample($s, $t);
     }
 
 
@@ -18,29 +23,121 @@ class Solution
      */
     function minWindow($s, $t)
     {
-        $arrayS = str_split($s);
-        $arrayT = str_split($t);
-
-        $find = [];
-        $result = [];
-        $resultString = [];
-        $string = '';
-        for ($i = 0; $i < count($arrayS); $i++) {
-            if (in_array($arrayS[$i], $arrayT)) {
-                print $arrayS[$i] . "\n";
-                $result[] = $arrayS[$i];
-                $find[$arrayS[$i]] = $arrayS[$i];
-                $string = $string . $arrayS[$i];
-                if (count($arrayT) == count($find)) {
-                    print $string;
-                    $resultString[] = $string;
-                }
-            }
-
-            $result[] = $arrayS[$i];
+        if (empty($s) || empty($t) || strlen($s) < strlen($t)){
+            return "";
         }
 
-        //var_dump($result);die;
-        return '';
+        if ($s == $t) {
+            return $s;
+        }
+        $need = [];
+        for($i=0; $i < strlen($t);$i++){
+            if (isset($need[$t[$i]])){
+                $need[$t[$i]]++;
+            } else {
+                $need[$t[$i]] = 1;
+            }
+        }
+
+        $right = 0;
+        $left = 0;
+
+        $have = [];
+        $needMatch = count($need);
+        $countMatch = 0;
+        $min = PHP_INT_MAX;
+        $res = "";
+
+        while ($right < strlen($s)) {
+            $s_right = $s[$right];
+
+            if (isset($have[$s_right])) {
+                $have[$s_right]++;
+            } else {
+                $have[$s_right] = 1;
+            }
+
+            if ($have[$s_right] == $need[$s_right]) {
+                $countMatch++;
+            }
+
+            while ($countMatch >= $needMatch) {
+                $res_lenght = $right - $left + 1;
+
+                if ($res_lenght < $min) {
+                    $min = $res_lenght;
+                    $res = substr($s, $left, $min);
+                }
+
+                $s_left = $s[$left];
+
+                $have[$s_left]--;
+                if ($have[$s_left] == $need[$s_left] - 1) {
+                    $countMatch--;
+                }
+
+                $left++;
+            }
+
+            $right++;
+        }
+        return $res;
+    }
+
+    /**
+     * @param String $s * @param String $t
+     * @return String
+     */
+    function minWindowExample($s, $t) {
+        $len_s = strlen($s);
+        $len_t = strlen($t);
+        if($s == $t){
+            return $t;
+        } else if( empty($s) || empty($t) || $len_s<$len_t ) {
+            return '';
+        } else {
+            $res = '';
+        }
+        $have = $need = array();
+        $left = $right = $match = 0;
+        for ($i = 0; $i < $len_t; $i++) {
+                $need[$t[$i]]++;
+        }
+
+        $need_match = count($need);
+        $min = PHP_INT_MAX;
+        while ($right < $len_s) {
+            $r_str = $s[$right];
+            if (isset($have[$r_str])) {
+                $have[$r_str]++;
+            } else {
+                $have[$r_str] = 1;
+            }
+
+            if ($have[$r_str] == $need[$r_str]) {
+                $match++;
+            }
+
+            while ($match >= $need_match) {
+                $res_len = $right - $left + 1;
+                if ($res_len < $min) {
+                    $min = $res_len;
+                    $res = substr($s, $left, $min);
+                }
+
+                $l_str = $s[$left];
+                $have[$l_str]--;
+
+                if ($have[$l_str] == $need[$l_str] - 1) {
+                    $match--;
+                }
+
+                $left++;
+            }
+
+            $right++;
+        }
+
+        return $res;
     }
 }
